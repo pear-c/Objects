2839adf95354f9880df54d639592bbef87a53f8d
pear_c <zxcv9676@naver.com>
2025-03-05 13:07:14 +0900

    Create TicketSeller Class

---
+++ src/main/java/Ticket_Selling_App/TicketSeller.java
@@ -0,0 +1,14 @@
+package Ticket_Selling_App;
+
+// 판매원 클래스
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
