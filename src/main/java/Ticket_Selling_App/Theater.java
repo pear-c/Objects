7bd903759ae3e755c86545ff8fa5bc3634041e68
pear_c <zxcv9676@naver.com>
2025-03-05 13:32:40 +0900

    Refactoring Theater, TicketSeller Class - (Down Coupling)

--- src/main/java/Ticket_Selling_App/Theater.java
+++ src/main/java/Ticket_Selling_App/Theater.java
@@ -9,17 +9,17 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        // 초대장이 있으면 입장권으로 교환
-        if(audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        }
-        else { // 초대장이 없으면 돈 주고 입장권 구매
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+//        // 초대장이 있으면 입장권으로 교환
+//        if(audience.getBag().hasInvitation()) {
+//            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//            audience.getBag().setTicket(ticket);
+//        }
+//        else { // 초대장이 없으면 돈 주고 입장권 구매
+//            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//            audience.getBag().minusAmount(ticket.getFee());
+//            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+//            audience.getBag().setTicket(ticket);
+//        }
     }
 }
 
--- src/main/java/Ticket_Selling_App/TicketSeller.java
+++ src/main/java/Ticket_Selling_App/TicketSeller.java
@@ -8,7 +8,21 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+//    public TicketOffice getTicketOffice() {
+//        return ticketOffice;
+//    }
+
+    public void sellTo(Audience audience) {
+        // 초대장이 있으면 입장권으로 교환
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        }
+        else { // 초대장이 없으면 돈 주고 입장권 구매
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
