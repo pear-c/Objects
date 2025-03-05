510359cd66925bdc3d0a8910e981cc9f6b51594c
pear_c <zxcv9676@naver.com>
2025-03-05 13:06:03 +0900

    Create TicketOffice Class

---
+++ src/main/java/Ticket_Selling_App/TicketOffice.java
@@ -0,0 +1,29 @@
+package Ticket_Selling_App;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 매표소 클래스
+public class TicketOffice {
+    private Long amount;    // 판매 금액
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public TicketOffice(Long amount, Ticket ... tickets) {
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+
+    // 티켓 판매 메서드 : 편의상 tickets 중 맨 처음 Ticket 반환
+    public Ticket getTicket() {
+        return tickets.remove(0);
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
