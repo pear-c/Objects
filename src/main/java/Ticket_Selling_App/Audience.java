dd1235017c681d6e6138c4861231cc5f1410fc83
pear_c <zxcv9676@naver.com>
2025-03-05 13:01:03 +0900

    Ticket Selling Application - Audience Class

---
+++ src/main/java/Ticket_Selling_App/Audience.java
@@ -0,0 +1,14 @@
+package Ticket_Selling_App;
+
+// 관람객 클래스
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
