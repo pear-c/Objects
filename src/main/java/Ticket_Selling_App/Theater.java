affbeeac7924fb567d4bfbfeeb6d22d018fd46af
pear_c <zxcv9676@naver.com>
2025-03-05 13:11:10 +0900

    Create Theater Class

---
+++ src/main/java/Ticket_Selling_App/Theater.java
@@ -0,0 +1,25 @@
+package Ticket_Selling_App;
+
+// 소극장 클래스
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        // 초대장이 있으면 입장권으로 교환
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        }
+        else { // 초대장이 없으면 돈 주고 입장권 구매
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+}
+
