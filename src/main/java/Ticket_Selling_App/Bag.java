577141e4a98c6558ada1a5b24c2ffd28c4267111
pear_c <zxcv9676@naver.com>
2025-03-05 12:32:56 +0900

    Ticket Selling Application - Bag Class 구현

--- src/main/java/Ticket_Selling_App/Bag.java
+++ src/main/java/Ticket_Selling_App/Bag.java
@@ -6,6 +6,33 @@ public class Bag {
     private Invitation invitation;  // 초대장
     private Ticket ticket;          // 티켓
 
-    public boolean hasTicket()
+    // 초대장 없는 관람객 가방
+    public Bag(long amount) {
+        this(null, amount);
+    }
+    // 초대장을 받은 관람객 가방
+    public Bag(Invitation invitation, long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
 
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
 }
