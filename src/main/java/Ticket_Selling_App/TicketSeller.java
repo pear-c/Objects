41eca0c3e4b19865b0505d8dbef388426f5d4858
pear_c <zxcv9676@naver.com>
2025-03-06 12:33:16 +0900

    Refactoring Theater - Audience, TicketSeller 수정 : Audience.getBag() 제거, sellTo() 수정

--- src/main/java/Ticket_Selling_App/TicketSeller.java
+++ src/main/java/Ticket_Selling_App/TicketSeller.java
@@ -9,16 +9,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        // 초대장이 있으면 입장권으로 교환
-        if(audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        }
-        else { // 초대장이 없으면 돈 주고 입장권 구매
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
